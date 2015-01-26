d3179618419ac028802d4e295e44c5e4fa53fa8c
Blake Merryman <blake@griffintechnology.com>
2015-01-26 14:35:54 -0600

    Log script is basically finished. Just need to add the append functionality & list

--- DailyLogger.playground/section-1.swift
+++ DailyLogger.playground/section-1.swift
@@ -2,6 +2,101 @@
 
 // MARK: - Constants
 
+import Foundation
+
+
+
+
+
+
+
+
+
+
+func checkDirectoryAndCreateFileForFileManager(fileManager: NSFileManager, directoryToCheck directory: String, fileToCreate fileName: String) -> Bool {
+    
+    if fileManager.fileExistsAtPath(directory) {
+        
+        let filePath = directory + "/\(fileName)"
+        
+        if !fileManager.fileExistsAtPath(filePath) {
+            
+            // Create an empty file!
+            
+            var writeError: NSError? = nil
+            ("" as NSString).writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding, error: &writeError)
+            
+            if writeError != nil {
+                println(writeError)
+                return false
+            }
+        }
+        return true
+    }
+    return false
+}
+
+//let logDirectory = NSHomeDirectoryForUser(NSUserName()).stringByAppendingPathComponent("Desktop")
+//let fileManager = NSFileManager.defaultManager()
+//let logReadyForWriting = checkDirectoryAndCreateFileForFileManager(fileManager, directoryToCheck: logDirectory, fileToCreate: "test1.md")
+
+
+
+
+
+
+
+func buildFileNameForDate(date: NSDate) -> String {
+    
+    // Handle Today's Date
+    
+    let today = NSDate()
+    
+    // Date Components for file name...
+    
+    let componentsToGrab: NSCalendarUnit = .YearCalendarUnit | .MonthCalendarUnit | .DayCalendarUnit
+    let dateComponents = NSCalendar.currentCalendar().components(componentsToGrab, fromDate: today)
+    
+    let numberFormatter = NSNumberFormatter()
+    numberFormatter.paddingCharacter = "0"
+    numberFormatter.minimumIntegerDigits = 2
+    
+    let year  = numberFormatter.stringFromNumber(dateComponents.year)!
+    let month = numberFormatter.stringFromNumber(dateComponents.month)!
+    let day   = numberFormatter.stringFromNumber(dateComponents.day)!
+    
+    return "\(year)-\(month)-\(day)-DL.md"
+}
+
+func buildLogHeader(#author: String, forDate date: NSDate) -> String {
+    
+    // Handle Today's Date
+    
+    let today = NSDate()
+    
+    // Date Formatting for header...
+    
+    let dateFormatter = NSDateFormatter()
+    dateFormatter.dateStyle = NSDateFormatterStyle.FullStyle
+    let todaysFullDate = dateFormatter.stringFromDate(today)
+    
+    return"# Daily Log of \(author)\n  *\(todaysFullDate)*\n\n--------------------------------------------------"
+}
+
+//let today = NSDate()
+//let filename = buildFileNameForDate(today)
+//let header = buildLogHeader(author: "Blake Merryman", forDate: today)
+
+
+
+
+
+
+
+
+
+
+
 let kError           = "Key: Errors are stored here. Should Check first."
 let kFlag            = "Key: Console Flag (normal version)"
 let kFlagShort       = "Key: Console Flag (short version)"
@@ -81,18 +176,17 @@ class ArgumentParser {
 
 // MARK: - Testing...
 
-let userInput1 = ["./DailyLogger.swift", "--help"]
-let userInput2 = ["./DailyLogger.swift","--append","This is some text"]
-
-let myFlags: [[String:Any]] = [
-    [kFlag: "--help", kFlagShort: "-h", kArgumentCount: 0],
-    [kFlag: "--list", kFlagShort: "-l", kArgumentCount: 0],
-    [kFlag: "--append", kFlagShort: "-a", kArgumentCount: 1]
-]
-
-let parser = ArgumentParser(flagsToParse: myFlags, inputToParse: userInput2)
-println(parser.parsedInput)
-
-
+//let userInput1 = ["./DailyLogger.swift", "--help"]
+//let userInput2 = ["./DailyLogger.swift","--append","This is some text"]
+//
+//let myFlags: [[String:Any]] = [
+//    [kFlag: "--help", kFlagShort: "-h", kArgumentCount: 0],
+//    [kFlag: "--list", kFlagShort: "-l", kArgumentCount: 0],
+//    [kFlag: "--append", kFlagShort: "-a", kArgumentCount: 1]
+//]
+//
+//let parser = ArgumentParser(flagsToParse: myFlags, inputToParse: userInput2)
+//println(parser.parsedInput)
 
 
+// --------------------------------------------------
--- DailyLogger.swift
+++ DailyLogger.swift
@@ -30,196 +30,157 @@ let textEditor = "Sublime Text 3.app"
 
 // Store this log in the following directory (relative to current user's home directory)...
 let logDirectory = NSHomeDirectoryForUser(NSUserName()).stringByAppendingPathComponent("Dropbox/DailyLog")
+let pathToHelpFile = NSHomeDirectoryForUser(NSUserName()).stringByAppendingPathComponent("Dropbox/Developer/MyTools/DailyLogger") + "/help.txt"
 
-// --------------------------------------------------
+
+// ----------------------------------------------------------------------------------------------------
 
 
-// MARK: - Log Options
+// MARK: - Utility Functions
 
-// Each option has a long- & short- flag value along with a completion handler to handle its arguments processed from user input.
+func buildFileNameForDate(date: NSDate) -> String {
+    
+    // Handle Today's Date
+    
+    let today = NSDate()
+    
+    // Date Components for file name...
+    
+    let componentsToGrab: NSCalendarUnit = .YearCalendarUnit | .MonthCalendarUnit | .DayCalendarUnit
+    let dateComponents = NSCalendar.currentCalendar().components(componentsToGrab, fromDate: today)
+    
+    let numberFormatter = NSNumberFormatter()
+    numberFormatter.paddingCharacter = "0"
+    numberFormatter.minimumIntegerDigits = 2
+    
+    let year  = numberFormatter.stringFromNumber(dateComponents.year)!
+    let month = numberFormatter.stringFromNumber(dateComponents.month)!
+    let day   = numberFormatter.stringFromNumber(dateComponents.day)!
+    
+    return "\(year)-\(month)-\(day)-DL.md"
+}
+
+func buildLogHeader(#author: String, forDate date: NSDate) -> String {
+    
+    // Handle Today's Date
+    
+    let today = NSDate()
+    
+    // Date Formatting for header...
+    
+    let dateFormatter = NSDateFormatter()
+    dateFormatter.dateStyle = NSDateFormatterStyle.FullStyle
+    let todaysFullDate = dateFormatter.stringFromDate(today)
+    
+    return"# Daily Log of \(author)\n  *\(todaysFullDate)*\n\n--------------------------------------------------"
+}
+
+func checkDirectoryAndCreateFileForFileManager(fileManager: NSFileManager, directoryToCheck directory: String, fileToCreate fileName: String, withContents fileContents: String) -> Bool {
+    
+    if fileManager.fileExistsAtPath(directory) {
+        
+        let filePath = directory + "/\(fileName)"
+        
+        if !fileManager.fileExistsAtPath(filePath) {
+            
+            // Create an empty file!
+            
+            var writeError: NSError? = nil
+            (fileContents as NSString).writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding, error: &writeError)
+            
+            if writeError != nil {
+                println("Error: Could not create file... \(writeError)")
+                return false
+            }
+        }
+        return true // Only returns TRUE if file is created successfully OR file already exists
+    }
+    return false
+}
+
+
+// MARK: - Setting Up The Log File For Usage...
+
+let today       = NSDate()
+let logFilename = buildFileNameForDate(today)
+let logHeader   = buildLogHeader(author: loggersName, forDate: today)
+let fileManager = NSFileManager.defaultManager()
+let logIsReady  = checkDirectoryAndCreateFileForFileManager(fileManager, directoryToCheck: logDirectory, fileToCreate: logFilename, withContents: logHeader)
+
+let fullPathToFile = "\(logDirectory)/\(logFilename)"
+
+
+// ----------------------------------------------------------------------------------------------------
+
+
+// MARK: - Define Options for User Input
+
+// Each option has a long- & short- flag value along with a completion handler to handle its results (and any errors) processed from user input.
 
 let help = Option(longFlag: "--help", shortFlag: "-h", completionHandler: { (result: Result!, error: NSError!) in 
 
-    println("Test - Helping") 
-
+    if error != nil {
+        println("Error performing completion handler! \(error)")
+    } 
+    else {
+        // Print the help file to the console using the cat command
+        let printHelpTask = NSTask()
+        printHelpTask.launchPath = "/bin/cat"
+        printHelpTask.arguments = [pathToHelpFile]
+        printHelpTask.launch()
+    }
 })
 
 let open = Option(longFlag: "--open", shortFlag: "-o", completionHandler: { (result: Result!, error: NSError!) in 
 
-    println("Test - Opening") 
-
+    if error != nil {
+        println("Error performing completion handler! \(error)")
+    } 
+    else {
+        NSWorkspace.sharedWorkspace().openFile("\(logDirectory)/\(logFilename)", withApplication: textEditor, andDeactivate: true)
+    }
 })
 
 let list = Option(longFlag: "--list", shortFlag: "-l", completionHandler: { (result: Result!, error: NSError!) in 
 
-    println("Test - Listing") 
-
+    if error != nil {
+        println("Error performing completion handler! \(error)")
+    } 
+    else {
+        println("Test - Listing") 
+    }
 })
 
 let append = Option(longFlag: "--append", shortFlag: "-a", completionHandler: { (result: Result!, error: NSError!) in
     
-    println("Test - Appending")
-
+    if error != nil {
+        println("Error performing completion handler! \(error)")
+    } 
+    else {
+        println("Test - Appending") 
+    }
 })
 
 
+// ----------------------------------------------------------------------------------------------------
 
-let logOptions = [help, open, list, append]
-let myParser = Parser(options: logOptions)
 
-if let results = myParser.parseArguments(Process.arguments) {
-    println("Args received")
+// MARK: - Process User Input
+
+if logIsReady {
+
+    let logOptions = [help, open, list, append]
+    let myParser = Parser(options: logOptions)
+
+    if let results = myParser.parseArguments(Process.arguments) {
+        // If we received arguments, each option's completion handler with perform the appropriate action!
+        println("DailyLogger: Arguments processed!")
+    }
+    else {
+        // If we do not receive any arguments from the user...
+        println("\n     DailyLogger currently requires some option to be specified. Use \"--help\" (or \"-h\") for more information.\n")
+    }
 }
 else {
-    println("Error!")
+    println("DailyLogger Error: Issues while preparing log file! Check to ensure that your destination path is correct and exists.")
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // MARK: - Define & Grab UserInput
-
-// enum UserInputOptions: String {
-//     case Help        = "help"
-//     case Append      = "--append"
-//     case AppendShort = "-a"
-//     case List        = "--list"
-//     case ListShort   = "-l"
-// }  
-
-// let arguments = Process.arguments
-
-// if arguments.count > 1 && arguments[1] == UserInputOptions.Help.rawValue {
-//     println("\n DailyLogger Help")
-//     println(" --------------------------------------------------")
-//     println("\n COMMANDS:")
-//     println("   --append (short -a): appends the trailing text to the end of daily log file")
-//     println("\n NOTES:")
-//     println("   - If you use punctuation in an appended note, make sure to properly escape.")
-//     println("   - ")
-//     println("")
-// } 
-// else {
-    
-//     // MARK: - Date & Log Header
-
-//     // Handle Today's Date
-
-//     let today = NSDate()
-
-//     // Date Formatting for header...
-
-//     let dateFormatter = NSDateFormatter()
-//     dateFormatter.dateStyle = NSDateFormatterStyle.FullStyle
-//     let todaysFullDate = dateFormatter.stringFromDate(today)
-
-//     // Date Components for file name...
-
-//     let componentsToGrab: NSCalendarUnit = .YearCalendarUnit | .MonthCalendarUnit | .DayCalendarUnit
-//     let dateComponents = NSCalendar.currentCalendar().components( componentsToGrab, fromDate: today)
-
-//     let numberFormatter = NSNumberFormatter()
-//     numberFormatter.paddingCharacter = "0"
-//     numberFormatter.minimumIntegerDigits = 2
-
-//     let year  = dateComponents.year
-//     let month = numberFormatter.stringFromNumber(dateComponents.month)!
-//     let day   = numberFormatter.stringFromNumber(dateComponents.day)!
-
-//     // Build Header for log file
-//     let logHeader = "# Daily Log of \(loggersName)\n  *\(todaysFullDate)*\n\n--------------------------------------------------"
-
-
-//     // MARK: - File Handling
-
-//     let fileManager = NSFileManager.defaultManager()
-
-//     // Check that our directory exists...
-
-//     if fileManager.fileExistsAtPath(logDirectory) {    
-
-//         let filePath = logDirectory + "/\(year)-\(month)-\(day)-DL.md"
-
-//         // Check if file already exists...
-//         if !fileManager.fileExistsAtPath(filePath) {
-
-//             // File does not yet exist so create it...
-//             var writeError: NSError? = nil
-//             (logHeader as NSString).writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding, error: &writeError)
-            
-//         } 
-
-
-//         // LIST
-//         if arguments.count > 1 && (arguments[1] == UserInputOptions.List.rawValue || arguments[1] == UserInputOptions.ListShort.rawValue) {
-            
-//             // Get current content of file & print...
-//             var readError: NSError? = nil
-//             println("\n\nLog Entry: \(filePath)")
-//             print(NSString(contentsOfFile: filePath, encoding: NSUTF8StringEncoding, error: &readError)!)
-//             println("\n\n") // Formatting
-        
-//         } 
-//         // APPEND
-//         else if arguments.count > 1 && (arguments[1] == UserInputOptions.Append.rawValue || arguments[1] == UserInputOptions.AppendShort.rawValue) {
-            
-//             var newLogEntry = "\n\n"
-
-//             for (index,argument) in enumerate(arguments) {
-//                 if index > 1 {
-//                     newLogEntry += argument + " "
-//                 }
-//             }
-
-//             // Get current content of file & append...
-//             var readError: NSError? = nil
-//             var fileContents = NSString(contentsOfFile: filePath, encoding: NSUTF8StringEncoding, error: &readError)! as String
-
-//             fileContents += newLogEntry
-
-//             // Write it all back to the disk!
-//             var writeError: NSError? = nil
-//             (fileContents as NSString).writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding, error: &writeError)
-        
-//         }
-//         // NONE... So OPEN
-//         else {
-
-//             // MARK: - Open File in Text Editor of Choice
-
-//             NSWorkspace.sharedWorkspace().openFile(filePath, withApplication: textEditor, andDeactivate: true)
-//         }
-//     } 
-    
-//     else {
-//         println("Error: Directory Does Not Exist")
-//     }
-
-// }
-
-
